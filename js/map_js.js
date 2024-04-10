e7f39042c10f077d2a33acc2cb357ce96dccb76e
Arod39 <arodriguez39@hawk.iit.edu>
2024-04-09 22:53:24 -0500

    Update 3 - Refining and Editing
    I think I got the js written down correctly, and I see that the console is returning the button that is properly selected. However, I think I am having some CSS issues so I will double check on that.
    
    Furthermore, I will keep revising to make sure everything seems correct.

--- js/map_js.js
+++ js/map_js.js
@@ -35,6 +35,14 @@ document.querySelectorAll(".image_container").forEach(image_container => {
     button.addEventListener("click",() => {
      //un-select all the items
      items.forEach(item => item.classList.remove("img--selected"));
+     buttons.forEach(button => button.classList.remove("image_button--selected"));
+     
+     items[i].classList.add("img--selected");
+     button.classList.add("image_button--selected");
     });
   });
+  
+  // Select the first item on page load
+  items[0].classList.add("img--selected");
+  buttons[0].classList.add("image_button--selected");
 });
