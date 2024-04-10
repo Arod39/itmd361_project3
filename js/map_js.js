1e365022da49626dcc58269edcd6912e4bcf999b
Arod39 <arodriguez39@hawk.iit.edu>
2024-04-09 20:09:09 -0500

    Attempt 3 - Map not loading
    Based on the error message I got on the last attempt, I switched out the API to the one I was able to generate with my Google account.
    
    However, I cannot manage this API because this feature is stuck behind a paywall that asks for information I don't want to fill out.
    
    I will try to generate a different API and see what happens.
    
    I also removed the link to CSS found in the head, because one of the errors was that the stylesheet was not found. Since I am trying to get this to work without copying any CSS, I will continue to work from this point forward.

--- js/map_js.js
+++ js/map_js.js
@@ -1,8 +1,8 @@
-function initMap() {
-map = new google.maps.Map(document.getElementById('map'), {
-center: {lat: 50.8476, lng: 4.3572},
-zoom: 8
-});
+  function initMap(){
+  var map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8
+    });
 }
 
 google.maps.event.addDomListener(window, 'load', initMap);
