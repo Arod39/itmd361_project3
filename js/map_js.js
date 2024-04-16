6f0581ec207b5476f2d44742cb80db3f600123a7
Arod39 <arodriguez39@hawk.iit.edu>
2024-04-16 15:31:16 -0500

    Update 7 - Only BillingError
    I've edited the HTML for the maps page and the only error im getting is the billing error, which I am not sure what to do for.
    
    I will have to see what to do with the js file as there is a possibility i might need to change it.

--- js/map_js.js
+++ js/map_js.js
@@ -1,9 +1,8 @@
-/*
 
 
 //Javascript for Maps API//
 
-
+/*
 let map;
 
 async function initMap() {
@@ -15,19 +14,39 @@ async function initMap() {
   });
 }
 
-google.maps.event.addDomListener(window, 'load', initMap);
+google.maps.event.addEventListener(window, 'load', initMap);
+
 
 */
 
-  function initMap(){
-  var map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: -34.397, lng: 150.644},
-      zoom: 8
-    });
+async function initMap() {
+  // Request needed libraries.
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+  const myLatlng = { lat: -25.363, lng: 131.044 };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: myLatlng,
+    mapId: "DEMO_MAP_ID",
+  });
+  const marker = new google.maps.marker.AdvancedMarkerElement({
+    position: myLatlng,
+    map,
+    title: "Click to zoom",
+  });
+
+  map.addListener("center_changed", () => {
+    // 3 seconds after the center of the map has changed, pan back to the
+    // marker.
+    window.setTimeout(() => {
+      map.panTo(marker.position);
+    }, 3000);
+  });
+  marker.addListener("click", () => {
+    map.setZoom(8);
+    map.setCenter(marker.position);
+  });
 }
-
-google.maps.event.addDomListener(window, 'load', initMap);
-
 
 //Javascript for Image Slider on Anime Page//
 
