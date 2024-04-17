838d0048ee65e4095a73fcc337038ddae705320f
Arod39 <arodriguez39@hawk.iit.edu>
2024-04-16 19:13:27 -0500

    Update 8 - Copied Prof Code - Errors
    I copied the exact code from the professor's demo (including CSS) and ended up receiving the same exact error regarding billing. Not sure what to do at this point

--- js/map_js.js
+++ js/map_js.js
@@ -1,84 +1,36 @@
+function init(){
+	//alert('it works');
+	var el = document.getElementById('canvas');
+	var myLocation = new google.maps.LatLng(41.835117, -87.627130);
+	var mapOptions = {
+		center: myLocation,
+		zoom: 18,
+		mapTypeId: google.maps.MapTypeId.SATELLITE,
+		mapTypeControlOptions: {
+			position: google.maps.ControlPosition.BOTTOM_CENTER
+		}
+	};
+
+	var myMap = new google.maps.Map(el, mapOptions);
+
+	var marker = new google.maps.Marker({
+		position: myLocation,
+		map: myMap,
+		animation: google.maps.Animation.BOUNCE,
+		icon: 'iit-icon.png'
+	});
+
+	var contentString = '<h1>IIT Perlstein Hall</h1><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate odit optio, voluptatem placeat odio dignissimos illo magnam esse asperiores voluptas at iure vero eum, nemo aperiam? Ipsam, atque nobis rem.</p>';
+
+	var infowindow = new google.maps.InfoWindow({
+      content: contentString
+  	});
+
+	google.maps.event.addListener(marker, 'mouseover', function() {
+    	infowindow.open(myMap, marker);
+  	});
 
 
-//Javascript for Maps API//
-
-/*
-let map;
-
-async function initMap() {
-  const { Map } = await google.maps.importLibrary("maps");
-
-  map = new Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
 }
 
-google.maps.event.addEventListener(window, 'load', initMap);
-
-
-*/
-
-async function initMap() {
-  // Request needed libraries.
-  const { Map } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-  const myLatlng = { lat: -25.363, lng: 131.044 };
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
-    center: myLatlng,
-    mapId: "DEMO_MAP_ID",
-  });
-  const marker = new google.maps.marker.AdvancedMarkerElement({
-    position: myLatlng,
-    map,
-    title: "Click to zoom",
-  });
-
-  map.addListener("center_changed", () => {
-    // 3 seconds after the center of the map has changed, pan back to the
-    // marker.
-    window.setTimeout(() => {
-      map.panTo(marker.position);
-    }, 3000);
-  });
-  marker.addListener("click", () => {
-    map.setZoom(8);
-    map.setCenter(marker.position);
-  });
-}
-
-//Javascript for Image Slider on Anime Page//
-
-document.querySelectorAll(".image_container").forEach((image_container) => {
-  const items = image_container.querySelectorAll(".slider_img");
-  const buttonsHTML = Array.from(items, () => {
-    return `<span class="image_button"></span>`;
-  });
-  
-  image_container.insertAdjacentHTML(
-  "beforeend", 
-  `
-    <div class="image_nav">
-      ${buttonsHTML.join("")}
-    </div>
-  `
- );
-    
-  const buttons = image_container.querySelectorAll(".image_button");
-  
-  buttons.forEach((button, i) => {
-    button.addEventListener("click",() => {
-     //un-select all the items
-     items.forEach((item) => item.classList.remove("slider_img--selected"));
-     buttons.forEach((button) => button.classList.remove("image_button--selected"));
-     
-     items[i].classList.add("slider_img--selected");
-     button.classList.add("image_button--selected");
-    });
-  });
-  
-  // Select the first item on page load
-  items[0].classList.add("slider_img--selected");
-  buttons[0].classList.add("image_button--selected");
-});
+google.maps.event.addDomListener(window, 'load', init);
