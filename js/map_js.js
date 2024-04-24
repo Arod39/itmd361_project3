e9e24e9acc9473fce20d899a1bfcfacaee663cd0
Arod39 <arodriguez39@hawk.iit.edu>
2024-04-23 22:13:13 -0500

    Map & Image Slider Finalized
    I made progress on the image slider and map. I am making this commit now before moving forward to the media queries.

--- js/map_js.js
+++ js/map_js.js
@@ -4,7 +4,7 @@ function init(){
 	var myLocation = new google.maps.LatLng(50.8260, 4.3802);
 	var mapOptions = {
 		center: myLocation,
-		zoom: 20,
+		zoom: 17,
 		mapTypeId: google.maps.MapTypeId.SATELLITE,
 		mapTypeControlOptions: {
 			position: google.maps.ControlPosition.BOTTOM_CENTER
@@ -17,10 +17,10 @@ function init(){
 		position: myLocation,
 		map: myMap,
 		animation: google.maps.Animation.DROP,
-		icon: 'maps_icon1'
+		icon: 'rsz_maps_icon1.png',
 	});
 
-	var contentString = '<h1>Brusells, Belgium</h1><p></p>';
+	var contentString = '<h1>Brusells, Belgium</h1><p>Brussels is Belgium’s capital and home to the European Union headquarters. The Grand-Place square at the heart of the city has shops and cafes inside ornate 17th-century guildhouses, and the intricate Gothic Hôtel de Ville (town hall) with a distinctive bell tower. The 19th-century Maison du Roi houses the Musée de la Ville de Bruxelles city-history museum, including costumes for the city’s famed Manneken Pis statue.</p>';
 
 	var infowindow = new google.maps.InfoWindow({
       content: contentString
@@ -29,7 +29,6 @@ function init(){
 	google.maps.event.addListener(marker, 'mouseover', function() {
     	infowindow.open(myMap, marker);
   	});
-
+  
 }
-
 google.maps.event.addDomListener(window, 'load', init);
